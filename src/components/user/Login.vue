5a2b680cc12afea764f824f81eefc9197e13e395
wang dong <wangdong@t2cloud.net>
2017-12-23 11:51:00 +0800

    Finished login vue

--- src/components/user/Login.vue
+++ src/components/user/Login.vue
@@ -1,89 +1,87 @@
 <template lang="pug">
-	.login
-		slider
-		.login-body
-			.login-content(class="_box")
-				h1 Alone
-				el-form(ref="form", :model="loginForm")
-					el-form-item
-						el-input(v-model="loginForm.name" placeholder="Username or phone number")
-					el-form-item
-						el-input(type="password" v-model="loginForm.password" placeholder="Password")
-					el-form-item
-						el-button(@click="submit(loginForm)" type="primary") Log in
-					el-form-item
-						el-button(type="text") Forgot password?
+  .login
+    slider
+    .login-body
+      .login-content(class="_box")
+        h1 Alone
+        el-form(ref="form", :model="loginForm")
+          el-form-item
+            el-input(v-model="loginForm.name" placeholder="Username or phone number")
+          el-form-item
+            el-input(type="password" v-model="loginForm.password" placeholder="Password")
+          el-form-item
+            el-button(@click="submit(loginForm)" type="primary") Log in
+          el-form-item
+            el-button(type="text") Forgot password?
 
-			.login-footer(class="_box")
-				p Don't have an account?
-					a Sign up
+      .login-footer(class="_box")
+        p Don't have an account?
+          a Sign up
 
 
 </template>
 
 
-
 <script>
-	import { mapActions } from 'vuex'
-	import Slider from './Slider'
+  import {mapActions} from 'vuex'
+  import Slider from './Slider'
 
-	export default {
+  export default {
 
-		data() {
-			return {
-				loginForm:{
-					name: "",
-					password: "",				
-				}
-			}
-		},
-		components:{
-	    Slider: Slider,
-  	},
+    data() {
+      return {
+        loginForm: {
+          name: "",
+          password: "",
+        }
+      }
+    },
+    components: {
+      Slider: Slider,
+    },
 
-		methods: {
-			...mapActions([
-				'login',
-			]),
+    methods: {
+      ...mapActions([
+        'login',
+      ]),
 
-			submit(loginForm) {
-				this.login(loginForm)
-			}
-		},
-	}
+      submit(loginForm) {
+        this.login(loginForm)
+      }
+    },
+  }
 
 </script>
 
 
 <style lang="stylus">
-	.login
-		display: flex
-		margin: 80px auto 0
+  .login
+    display: flex
+    margin: 80px auto 0
 
-		._box
-				background-color: #fff
-				border: 1px solid #e6e6e6
-				border-radius: 1px
-				width: 340px
-		.login-body
-			 margin-top: 100px	
-			.login-content
-				h1	
-					font-family:"Trebuchet MS", Arial, Helvetica, sans-serif
-					text-align: center
-				.el-form
-					width: 240px
-					margin: 0 auto
-					.el-form-item
-						.el-button
-							width: 240px
+    ._box
+      background-color: #fff
+      border: 1px solid #e6e6e6
+      border-radius: 1px
+      width: 340px
+    .login-body
+      margin-top: 100px
+    .login-content
+      h1
+        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif
+        text-align: center
+      .el-form
+        width: 240px
+        margin: 0 auto
+        .el-form-item
+          .el-button
+            width: 240px
 
-
-			.login-footer
-				margin: 20px 0
-				p
-					text-align: center
-					a
-						margin-left: 5px 
+    .login-footer
+      margin: 20px 0
+      p
+        text-align: center
+        a
+          margin-left: 5px
 
 </style>
--- src/components/user/Slider.vue
+++ src/components/user/Slider.vue
@@ -1,23 +1,75 @@
 <template lang="pug">
-	.box
-		.picture
-	
+  .box
+    .picture
+      transition-group(name="images")
+        img(v-for="(image, index) in images" :key="index" :src="image" v-show="index===mark")
+
 </template>
 
 <script>
-  export default{
+  export default {
     name: 'silder',
-     data:() => {}
-   }
+
+    data(){
+      return {
+        mark: 0,
+        images: [
+          '/static/picture/001bc33056c1.jpg',
+          '/static/picture/2d9d7248af43.jpg',
+          '/static/picture/629d23a3c7b2.jpg',
+          '/static/picture/aafd8c6b005d.jpg',
+          '/static/picture/f5ae123ab1e2.jpg'
+        ]
+      }
+    },
+
+    methods: {
+      autoPlay(){
+        var _this = this
+        setInterval(function () {
+          _this.mark++;
+          if (_this.mark == 4) {
+            _this.mark = 0
+          }
+        }, 2500)
+      }
+    },
+
+
+    created() {
+      this.autoPlay()
+    }
+  }
 
 </script>
 
 
 <style lang="stylus">
-	.box
-		background-image: url('/static/picture/f82c2ede4ccb.png')
-		background-size: 454px 618px
-		height: 618px
-		width: 454px
+  .box
+    background-image: url('/static/picture/f82c2ede4ccb.png')
+    background-size: 454px 618px
+    height: 618px
+    width: 454px
+
+    .picture
+      margin: 99px 0 0 151px
+      img
+        position: absolute
+
+    .images-enter-active
+      transform: translateX(0)
+      transition: all 1.5s ease
+
+    .image-leave-active
+      transform: translateX(-100%)
+      transition: all 1.5s ease
+
+    .image-enter
+      transform: translateX(100%)
+
+    .image-leave
+      transform: translateX(0);
+
+
 </style>
 
